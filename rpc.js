1d7a7f783bba50a1b0e523247c35387c3f174f2d
Jacob <jacoblittler2003@gmail.com>
2025-11-10 14:24:27 -0500

    Added basic UI functionality to the RPC. Practiced manipulating the DOM and git branching.

--- rpc.js
+++ rpc.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let compScore = 0;
+
 function getComputerChoice() {
   const values = ["rock", "paper", "scissors"];
 
@@ -13,10 +16,37 @@ function getHumanChoice() {
 }
 
 function playGame() {
-  let humanScore = 0;
-  let compScore = 0;
+  // UI
+  const container = document.createElement("div");
+  const rockBtn = document.createElement("button");
+  const scissorsBtn = document.createElement("button");
+  const paperBtn = document.createElement("button");
+  const results = document.createElement("div");
+  const winner = document.createElement("p");
 
-  let playRound = function playRound(humanChoice, compChoice) {
+  rockBtn.textContent = "Rock";
+  scissorsBtn.textContent = "Scissors";
+  paperBtn.textContent = "Paper";
+  winner.textContent = "";
+
+  document.body.appendChild(container);
+  container.appendChild(rockBtn);
+  container.appendChild(scissorsBtn);
+  container.appendChild(paperBtn);
+  container.appendChild(results);
+  container.appendChild(winner);
+
+  rockBtn.addEventListener("click", () =>
+    playRound("rock", getComputerChoice())
+  );
+  scissorsBtn.addEventListener("click", () =>
+    playRound("scissors", getComputerChoice())
+  );
+  paperBtn.addEventListener("click", () =>
+    playRound("paper", getComputerChoice())
+  );
+
+  function playRound(humanChoice, compChoice) {
     humanChoice = humanChoice.toLowerCase();
     let winner = "";
     let loser = "";
@@ -25,30 +55,43 @@ function playGame() {
       winner = humanChoice;
       loser = compChoice;
       ++humanScore;
-      console.log("You Win! " + winner + " beats " + loser + ".");
+      results.innerText = "You Win! " + winner + " beats " + loser + ".";
     } else if (humanChoice === "rock" && compChoice === "scissors") {
       winner = humanChoice;
       loser = compChoice;
       ++humanScore;
-      console.log("You Win! " + winner + " beats " + loser + ".");
+      results.innerText = "You Win! " + winner + " beats " + loser + ".";
     } else if (humanChoice === "paper" && compChoice === "rock") {
       winner = humanChoice;
       loser = compChoice;
       ++humanScore;
-      console.log("You Win! " + winner + " beats " + loser + ".");
+      results.innerText = "You Win! " + winner + " beats " + loser + ".";
     } else {
       winner = compChoice;
       loser = humanChoice;
       ++compScore;
-      console.log("You lose! " + winner + " beats " + loser + ".");
+      results.innerText = "You lose! " + winner + " beats " + loser + ".";
     }
-    console.log("Player Score: " + humanScore + "Computer Score: " + compScore);
-  };
+    results.innerText =
+      "Player Score: " + humanScore + " Computer Score: " + compScore;
 
-  for (let i = 0; i < 5; ++i) {
-    const humanSelection = getHumanChoice();
-    const compSelection = getComputerChoice();
-    playRound(humanSelection, compSelection);
+    announceWinner();
+  }
+
+  function announceWinner() {
+    if (humanScore === 5) {
+      winner.innerText = "The Player is the winner!";
+      disableButtons();
+    } else if (compScore === 5) {
+      winner.innerText = "The Computer is the winner!";
+      disableButtons();
+    }
+  }
+
+  function disableButtons() {
+    rockBtn.disabled = true;
+    scissorsBtn.disabled = true;
+    paperBtn.disabled = true;
   }
 }
 
